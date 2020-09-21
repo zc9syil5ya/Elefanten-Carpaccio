f1693c492e6f4038da4819e04b39e13c45cc1517
Christian Sommer <chregi.sommer@gmail.com>
2020-09-21 09:31:44 +0200

    add "bestellwert eingeben"

--- src/ch/fhnw/sepc/euclac.java
+++ src/ch/fhnw/sepc/euclac.java
@@ -1,10 +1,21 @@
 package ch.fhnw.sepc;
 
+import javax.swing.*;
+import java.util.Scanner;
+
 public class euclac {
 
 
+ void prt(){
+     Scanner scanner = new Scanner(System.in);
+     System.out.print("Bestellwert eingeben: ");
+     String bs = scanner.next();
+ }
+
+
     public static void main(String[] args) {
-        
+        euclac ec = new euclac();
+        ec.prt();
     }
 
 
